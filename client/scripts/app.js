18e582e19ad5ccf8a9fc6cd1889c0e84f41309d5
Heylisse Machado <hmachad1@my.centennialcollege.ca>
2021-10-04 18:53:55 -0400

    Front end code is initalized

---
+++ client/scripts/app.js
@@ -0,0 +1,8 @@
+'use strict';
+(() => {
+    function Start() {
+        console.log('App Front End Started');
+    }
+    window.addEventListener('load', Start);
+})();
+//# sourceMappingURL=apps.js.map
