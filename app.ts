7c463d00b24913b7920f0db5f2b8f21bbb7155c7
Heylisse Machado <hmachad1@my.centennialcollege.ca>
2021-10-20 00:42:05 -0400

    first commit

--- app.ts
+++ app.ts
@@ -3,9 +3,18 @@ import express from 'express';
 import path from 'path';
 import cookieParser from "cookie-parser";
 import logger from 'morgan';
+import mongoose from 'mongoose';
 
 import indexRouter from './routes/index';
 
+// instanciate mongo 
+mongoose.connect('mongodb://localhost:27017/shoes')
+
+const db = mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error'));
+db.once('open', function(){
+  console.log('connected to MongoDB at:mongodb://localhost:27017/shoes');
+})
 
 // instanciate express app
 const app = express();
